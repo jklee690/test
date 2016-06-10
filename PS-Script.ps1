1aebafd23def3c7a83ba695da266281717efb468
jklee690 <jklee690@cyberlogitec.com>
2016-06-10 09:56:37 +0000

    Add files via upload

---
+++ PS-Script.ps1
@@ -0,0 +1,24 @@
+function Extract-Zip
+{
+  param([string]$zipfilename, [string] $destination)
+
+  if(test-path($zipfilename))
+  {  
+    $shellApplication = new-object -com shell.application
+    $zipPackage = $shellApplication.NameSpace($zipfilename)
+    $destinationFolder = $shellApplication.NameSpace($destination)
+    $destinationFolder.CopyHere($zipPackage.Items())
+  }
+}
+
+Stop-Service tomcat8
+
+Rename-Item C:\WEB_MAIN C:\WEB_MAIN_Today
+
+Remove-Item C:\tomcat_work
+
+New-Item C:\WEB_MAIN -type directory
+
+Extract-Zip C:\WEB_MAIN.zip C:\WEB_MAIN
+
+Start-Service tomcat8
