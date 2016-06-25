d7603ff910c6539791fb9bf10c1e25aae39974f7
jklee690 <jklee690@cyberlogitec.com>
2016-06-25 11:51:55 +0000

    Add files via upload

--- PS-Script.ps1
+++ PS-Script.ps1
@@ -11,14 +11,76 @@ function Extract-Zip
   }
 }
 
-Stop-Service tomcat8
+$tomcatname = Get-Service -name "tomcat8" -ErrorAction SilentlyContinue | select object-name -ExpandProperty Name 
+if($tomcatname -ne "tomcat8")
+{
+		$tomcatname = Get-Service -name "tomcat7" -ErrorAction SilentlyContinue | select object-name -ExpandProperty Name 
+		if($tomcatname -ne "tomcat7")
+		{
+			write-output "no tomcat7 and tomcat 8 service"
+			exit 0
+		}
+}
 
-Rename-Item C:\WEB_MAIN C:\WEB_MAIN_Today
+$tomcatStatus = Get-Service -name $tomcatname | select object-name -ExpandProperty Status
 
-Remove-Item C:\tomcat_work
+if($tomcatStatus -eq "Running")
+{
+  Stop-Service $tomcatname
+}
 
-New-Item C:\WEB_MAIN -type directory
+$tomcatStatus = Get-Service -name $tomcatname | Where-Object {$_.Status -eq "Active"} | select object-name -ExpandProperty Status
+if($tomcatStatus -eq "Active")
+{
+	write-output "not enable stop tomcat7 and tomcat 8 service"
+	exit 0
+}
+$curdate = get-date -format yyMMdd
 
-Extract-Zip C:\git\WEB_MAIN.zip C:\WEB_MAIN
+$rename_path = "C:\clt\OPUS_FWD_2014\WEB_MAIN_" + $curdate
+$IsDir = test-path $rename_path
 
-Start-Service tomcat8
+if($IsDir)
+{
+    Remove-Item $rename_path -Recurse
+}
+
+
+Rename-Item "C:\clt\OPUS_FWD_2014\WEB_MAIN" $rename_path
+
+if($tomcatname -eq "tomcat8")
+{
+ if( test-path "C:\clt\Server\Tomcat 8.0\work")
+ {
+    Remove-Item "C:\clt\Server\Tomcat 8.0\work" -Recurse
+    Write-Output "remove tomcat8 work directory"
+  }
+}
+else
+{
+   if( test-path "C:\clt\Server\Tomcat 7.0\work")
+   {
+     Remove-Item "C:\clt\Server\Tomcat 7.0\work" -Recurse
+     Write-Output "remove tomcat7 work directory"
+   }
+}
+
+
+Extract-Zip C:\git\WEB_MAIN.zip "C:\clt\OPUS_FWD_2014"
+
+if( test-path "C:\clt\OPUS_FWD_2014\WEB_MAIN")
+{
+    Write-Output "create web_main directory"
+}
+else
+{
+    Write-Output "not create web_main directory"
+}
+
+Start-Service $tomcatname
+$tomcatStatus = Get-Service -name $tomcatname | Where-Object {$_.Status -eq "Active"} | select object-name -ExpandProperty Status
+if($tomcatStatus -eq "Stopped")
+{
+	write-output "not enable Active tomcat7 and tomcat 8 service"
+	exit 0
+}
