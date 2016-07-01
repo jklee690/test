902f861e6ae7a1e3496be2551c0ad323124607ef
jklee690 <jklee690@cyberlogitec.com>
2016-07-01 12:27:59 +0900

    dfdf

--- sql.ps1
+++ sql.ps1
@@ -4,16 +4,31 @@ $pwd = "fms"
 $database = "demofwdv401_test"
 $connectionString = "Server=$dataSource;uid=$user; pwd=$pwd;Database=$database;Integrated Security=False;"
 
-$query = get-content "c:\git\Patch.sql"
+$query = get-content "c:\git\Patch_Sql.sql"
 
 $connection = New-Object System.Data.SqlClient.SqlConnection
 $connection.ConnectionString = $connectionString
 #$connection.ConnectionString = "Server=$dataSource;Database=$database;Integrated Security=True;"
 $connection.Open()
 $command = $connection.CreateCommand()
-$command.CommandText = $query
 
-$command.ExecuteNonQuery()
+ $ScriptList = [regex]::Split($query, '\bGO')
+
+foreach ($sql in $ScriptList)
+{
+        
+		$sql.Trim()
+		
+		if($sql){		
+		
+			$command.CommandText = $sql
+			$command.ExecuteNonQuery()
+			Write-Output $sql
+
+			Write-Output 'SQL commit'
+		}
+
+}
 
 
 $connection.Close()
